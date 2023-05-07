f76cc42189f2ef4db84792fb18f39e3f825bd4f7
Peter Newman <peternewman@users.noreply.github.com>
2023-05-07 05:38:13 +0100

    Return the path and add methods to get a device by path or id (#5)
    * Fix test syntax for ShuttlePro
    
    * Return the path and add methods to get a device by path or id
    
    * Test the other functions and be consistent about returned data if it doesn't match
    
    * Validate we've got the right raw HID device
    
    * Enums for VID and PID
    
    * Added enums for VID and PID, and exposing them from Shuttle.js.
    
    * Hotplug issue on Linux
    
    * Fixed hotplug issue on Linux (issue #7).
    
    * Updated documentation to reflect changes in this PR.
    
    ---------
    
    Co-authored-by: James Hope <hopejr@gmail.com>

--- tests/test.js
+++ tests/test.js
@@ -3,19 +3,28 @@ const shuttle = require('../lib/Shuttle')
 
 shuttle.on('connected', (deviceInfo) => {
   console.log('Starting tests')
-  console.log('Connected', deviceInfo.id, deviceInfo.name)
+  console.log('Connected', deviceInfo.id, deviceInfo.path, deviceInfo.name)
   test('shuttle test', (t) => {
-    t.plan(3)
+    t.plan(11)
 
     t.equal(deviceInfo.hasShuttle, true)
     t.equal(deviceInfo.hasJog, true)
     if (deviceInfo.name === 'ShuttleXpress') {
       t.equal(deviceInfo.numButtons, 5)
     } else if (deviceInfo.name === 'ShuttlePro v1') {
-      t.equal(deviceInfo.numButtons === 13)
+      t.equal(deviceInfo.numButtons, 13)
     } else if (deviceInfo.name === 'ShuttlePro v2') {
-      t.equal(deviceInfo.numButtons === 15)
+      t.equal(deviceInfo.numButtons, 15)
     }
+    t.deepEqual(shuttle.getDeviceById(deviceInfo.id), deviceInfo)
+    t.equal(shuttle.getDeviceById("foo"), null)
+    t.deepEqual(shuttle.getDeviceByPath(deviceInfo.path), deviceInfo)
+    t.equal(shuttle.getDeviceByPath("/foo/bar"), null)
+    t.notLooseEqual(shuttle.getRawHidDevice(deviceInfo.id), null)
+    // Validate we've got the right raw HID device
+    t.match(shuttle.getRawHidDevice(deviceInfo.id).getDeviceInfo().manufacturer, /Contour/i)
+    t.match(shuttle.getRawHidDevice(deviceInfo.id).getDeviceInfo().product, /Shuttle/i)
+    t.equal(shuttle.getRawHidDevice("foo"), null)
     console.log('Unplug device')
   })
 })
